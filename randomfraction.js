39c4228238001ad96360d5e6d0633b10c95bbdba
Facundo <68478751+FacuAranda1@users.noreply.github.com>
2021-09-06 20:06:39 -0300

    -
    Random fraction with js

---
+++ randomfraction.js
@@ -0,0 +1,14 @@
+ function randomFraction() {
+
+  var resultado = 0;
+   while (resultado === 0){ 
+   resultado = Math.random();
+   }
+     return resultado; 
+  }
+
+ console.log(randomFraction());
+
+
+
+
